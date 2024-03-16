d570cbec68840a4cc2bf0599c67a248bc7b1affd
zakarya <zakaria.elkhiyati@gmail.com>
2024-03-16 09:47:34 +0100

    Add new environment registrations

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,13 +1,30 @@
-from .base_env import LVMCBaseEnv
-
 from .episodic.navigate_T import NavigateT
+from .continuous.channel import Channel
+from .continuous.channel_flow import ChannelFlow
+from gymnasium.envs.registration import register
 
 
 def make(env_name, **kwargs):
     envs = {
-        "NavigateT": NavigateT,
+        "NavigateT-v0": NavigateT,
     }
     try:
         return envs[env_name](**kwargs)
     except KeyError:
         raise ValueError(f"Unknown environment name {env_name}")
+
+
+register(
+    id="NavigateT-v0",
+    entry_point="envs.episodic.navigate_T:NavigateT",
+)
+
+register(
+    id="Channel-v0",
+    entry_point="envs.continuous.channel:Channel",
+)
+
+register(
+    id="ChannelFlow-v0",
+    entry_point="envs.continuous.channel_flow:ChannelFlow",
+)
