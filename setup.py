a87b88eee2f3303d860910cc85675bc9f51bd1b9
zakarya <zakaria.elkhiyati@gmail.com>
2024-03-16 09:47:08 +0100

    Fix aesthetics

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='vlmc_envs',
-    version='0.1',
+    name="vlmc_envs",
+    version="0.1",
     install_requires=[
-        'gymnasium',
-        'numpy',
+        "gymnasium",
+        "numpy",
     ],
 )
