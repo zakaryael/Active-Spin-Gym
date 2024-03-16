c00548d1cce4f2f0ddd214b5eddb1d0d64611633
zakarya <zakaria.elkhiyati@gmail.com>
2024-03-16 09:49:09 +0100

    Update base environment

--- envs/base_env.py
+++ envs/base_env.py
@@ -6,7 +6,12 @@ from lvmc.core.simulation import Simulation
 from lvmc.core.particle_lattice import Orientation
 from typing import Tuple, Optional
 import sys
-
+from rich.live import Live
+from rich.console import Console
+from rich.text import Text
+from rich.layout import Layout
+from rich.panel import Panel
+from IPython.display import clear_output, display
 
 
 class LVMCBaseEnv(gym.Env):
@@ -30,9 +35,10 @@ class LVMCBaseEnv(gym.Env):
         width: int,
         height: int,
         density: float,
-        control_interval: float = 1e-4,
-        g: float = 1.0,
+        control_interval: float = 1e-3,
+        g: float = 1.5,
         v0: float = 100,
+        max_iterations: Optional[int] = int(1e5),
     ) -> None:
         """
         Initialize the LVMC base environment.
@@ -53,15 +59,35 @@ class LVMCBaseEnv(gym.Env):
         self.density = density
         self.control_interval = control_interval
         self.current_time = 0.0
+        self.max_iterations = 10000
+        self.current_iteration = 0
 
         # Define the lattice topology.
-        self.obstacles = torch.zeros((self.height, self.width), dtype=torch.bool)
-        self.sinks = torch.zeros((self.height, self.width), dtype=torch.bool)
+        self._set_topology()
 
         self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(
-            low=0, high=1, shape=(4, self.height, self.width), dtype=np.bool_
+            low=0, high=1, shape=(4, self.height, self.width), dtype=np.uint8
         )
+        self.cumulative_reward = 0.0
+        self.max_iterations = max_iterations
+        self._initialize_simulation()
+
+        self.console = Console(record=True, force_terminal=True)
+        self.live = None  # Placeholder for the Live instance
+
+        print(
+            f"Initialized LVMCBaseEnv with width={width}, height={height}, density={density}, control_interval={control_interval}, g={g}, v0={v0}, max_iterations={max_iterations}"
+        )
+
+    def _set_topology(self):
+        """
+        Set the topology of the environment.
+        """
+        topology = self.compute_lattice_topology()
+
+        for key, value in topology.items():
+            setattr(self, key, value)
 
     def _initialize_simulation(self) -> None:
         """
@@ -75,7 +101,11 @@ class LVMCBaseEnv(gym.Env):
             v0=self.v0,
         )
         self.simulation.lattice.set_obstacles(self.obstacles)
-        self.simulation.lattice.set_sinks(self.sinks)
+        try:
+            self.simulation.lattice.set_sinks(self.sinks)
+            self.simulation.lattice.set_sources(self.sources)
+        except AttributeError:
+            pass
 
     def step(self, action: int) -> Tuple[torch.Tensor, float, bool, dict]:
         """
@@ -90,6 +120,8 @@ class LVMCBaseEnv(gym.Env):
 
         clock = 0.0
         while clock < self.control_interval:
+            if self.is_done():
+                break
             self.simulation.run()
             clock += self.simulation.delta_t
 
@@ -97,40 +129,126 @@ class LVMCBaseEnv(gym.Env):
         self.simulation.apply_magnetic_field(action - 1)
 
         self.current_time = self.simulation.t
+        self.current_iteration += 1
 
         reward = self.reward()
+        self.cumulative_reward += reward
         done = self.is_done()
-        info = {"time": self.current_time, "action": action}
+        self.info = {
+            "time": self.current_time,
+            "action": action,
+            "density": self.simulation.lattice.density,
+            "reward": reward,
+            "cumulative_reward": self.cumulative_reward,
+            "iteration": self.current_iteration,
+            "max_iterations": self.max_iterations,
+            "n_particles": self.simulation.lattice.n_particles,
+            "n_particles_right": self.simulation.lattice.particles[
+                Orientation.RIGHT.value
+            ]
+            .sum()
+            .item(),
+            "n_particles_left": self.simulation.lattice.particles[
+                Orientation.LEFT.value
+            ]
+            .sum()
+            .item(),
+        }
+        truncated = self.current_iteration >= self.max_iterations
+        return self._get_obs(), reward, done, truncated, self.info
 
-        return self._get_obs(), reward, done, info
-
-    def reset(self, seed=None) -> torch.Tensor:
+    def reset(self, seed=None, options=None) -> torch.Tensor:
         """
         Reset the environment to an initial state.
-
+        :param seed: The random seed to use for the environment.
+        :param options: Additional options for the environment.
         :return: The initial observation of the environment.
         """
         if seed is not None:
             np.random.seed(seed)
         self._initialize_simulation()
-        return self._get_obs()
 
-    def render(self, mode: str = "console") -> None:
-        """
-        Render the environment to the screen or other mode.
+        obs = self._get_obs()
+        return obs, {}
 
-        :param mode: The mode to render with.
-        """
+    def render(self, mode="console"):
         if mode == "console":
-            print('\033[2J\033[H', end='') # clear the terminal
-            sys.stdout.write(f'\r{self.simulation.lattice.__str__()}\033[K')
-            sys.stdout.flush() 
+            if self.live is None:
+                # Initialize the Live object if it hasn't been already
+                self.live = Live(console=self.console, auto_refresh=True)
+                self.live.start()
+
+            # Generate the content to be displayed
+            content = self._generate_render_content()
+            # Update the Live display
+            self.live.update(content)
+        if mode == "notebook":
+            # Generate the content to be displayed
+            content = self._generate_render_content()
+            # Display the content in the notebook
+            display(content)
+            clear_output(wait=True)
+
+    def _generate_render_content(self):
+        # Generate the lattice visualization, assuming it returns Rich-formatted text
+        lattice_visualization = self.simulation.lattice.visualize_lattice()
+        lattice_text = Text.from_markup(lattice_visualization)
+
+        # Generate additional information as Rich Text. all the info is stored in self.info in different colors
+        info_text = Text.assemble(
+            Text(f"Time: {self.info['time']:.2f}", style="bold white"),
+            Text(f" | "),
+            Text(f"Action: {self.info['action']}", style="bold orange"),
+            Text(f" | "),
+            Text(f"Density: {self.info['density']:.2f}", style="bold pink"),
+            Text(f" | "),
+            Text(f"Iteration: {self.info['iteration']}", style="bold white"),
+            Text(f" | "),
+            Text(f"Reward: {self.info['reward']:.2f}", style="blue"),
+            Text(f" | "),
+            Text(
+                f"Cumulative Reward: {self.info['cumulative_reward']:.2f}",
+                style="bold blue",
+            ),
+            Text(f" | "),
+            Text(
+                f"Max Iterations: {self.info['max_iterations']}", style="bold magenta"
+            ),
+            Text(f" | "),
+            Text(f"Particles: {self.info['n_particles']}", style="bold green"),
+            Text(f" | "),
+            Text(
+                f"Particles Right: {self.info['n_particles_right']}", style="bold green"
+            ),
+            Text(f" | "),
+            Text(
+                f"Particles Left: {self.info['n_particles_left']}", style="bold green"
+            ),
+        )
+
+        # Create the layout and add the lattice visualization and info text to it
+        layout = Layout()
+        layout.split_column(
+            Layout(name="main", ratio=3),  # The main lattice visualization
+            Layout(name="footer"),  # The footer with statistics
+        )
+
+        # Create panels for both sections
+        lattice_panel = Panel(lattice_text, title="Lattice Visualization")
+        info_panel = Panel(info_text, title="Statistics")
+
+        # Update panels with content
+        layout["main"].update(lattice_panel)
+        layout["footer"].update(info_panel)
+
+        return layout
 
     def close(self) -> None:
         """
         Perform any necessary cleanup.
         """
-        pass
+        if self.live is not None:
+            self.live.stop()  # Stop the live display
 
     def _get_obs(self) -> torch.Tensor:
         """
@@ -138,7 +256,7 @@ class LVMCBaseEnv(gym.Env):
 
         :return: The current observation of the environment.
         """
-        obs = self.simulation.lattice.query_lattice_state().numpy().astype(np.bool_)
+        obs = self.simulation.lattice.query_lattice_state().numpy().astype(np.uint8)
 
         return obs
 
@@ -155,5 +273,15 @@ class LVMCBaseEnv(gym.Env):
         Check if the environment is in a terminal state.
 
         :return: A boolean indicating if the environment is in a terminal state.
+        """
+        pass
+
+    def compute_lattice_topology(self) -> dict:
+        """
+        Compute the lattice topology for the environment.
+
+        :param width: The width of the lattice.
+        :param height: The height of the lattice.
+        :return: A dictionary containing the obstacles and sinks in the lattice.
         """
         pass
