4bd44920383bcc5b3dfd0d4ea7579162acc8c886
zakarya <zakaria.elkhiyati@gmail.com>
2024-07-20 16:09:06 +0200

    Refactor learning script

--- examples/sb_script.py
+++ examples/sb_script.py
@@ -1,88 +1,150 @@
 import os
 from envs import make
-from envs.episodic.navigate_T import NavigateT
-from stable_baselines3 import PPO
+from envs.episodic.navigate_T import NavigateT, EnvConfig
+from stable_baselines3 import PPO, DQN
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.evaluation import evaluate_policy
 
 import wandb
 from wandb.integration.sb3 import WandbCallback
 
-# Define your training parameters
-config = {
-    "algorithm": "PPO",
-    "policy_type": "MlpPolicy",
-    "n_total_timesteps": 10,
-    "n_envs": 16,
-    "gamma": 0.99,
-}
+import argparse
+from dataclasses import asdict
+import json
 
-# Initialize WandB
-run = wandb.init(
-    project="lvmc-channel-flow-v0",  # Make sure to replace with your actual project name
-    config=config,
-    sync_tensorboard=True,  # Auto-upload sb3's tensorboard metrics
-    monitor_gym=True,  # Auto-upload the videos of agents playing the game
-    save_code=True,  # Optional: This will save your script in WandB for versioning
-)
+# wandb.require("core")
 
-# Create the environment
-env = make_vec_env(
-    "ChannelFlow-v0",
-    n_envs=config["n_envs"],
-    env_kwargs={
-        "width": 50,
-        "height": 25,
-        "density": 0.3,
-        "control_interval": 1e-2,
-        "g": 1.5,
-        "v0": 100,
-        "max_iterations": int(1e6),
-    },
-)
 
-# Setup directories based on WandB run ID
-experiment_path = os.path.join("./data", run.id)
-os.makedirs(experiment_path, exist_ok=True)
+def main(args):
+    config = {
+        "algorithm": args.algorithm,
+        "policy_type": args.policy_type,
+        "n_total_timesteps": args.total_timesteps,
+        "n_envs": args.n_envs,
+        "gamma": args.gamma,
+        "env_config": {
+            "width": args.width,
+            "height": args.height,
+            "control_interval": args.control_interval,
+            "g": args.gravity,
+            "v0": args.initial_velocity,
+            "max_iterations": args.max_iterations,
+        },
+        "checkpoint_freq": args.checkpoint_freq,
+    }
 
-logs_path = os.path.join(experiment_path, "logs")
-os.makedirs(logs_path, exist_ok=True)
+    # Initialize WandB
+    run = wandb.init(
+        project=args.project,
+        config=config,
+        sync_tensorboard=True,  # Auto-upload sb3's tensorboard metrics
+        save_code=True,
+    )
+    # Make the environment configuration
+    env_config = EnvConfig(
+        width=args.width,
+        height=args.height,
+        control_interval=args.control_interval,
+        g=args.gravity,
+        v0=args.initial_velocity,
+        max_iterations=args.max_iterations,
+        env_log_dir=wandb.run.dir,
+    )
+    # Save the environment configuration
+    with open("env_config.json", "w") as f:
+        json.dump(asdict(env_config), f)
+    # Create the environment
+    env = make_vec_env(
+        "NavigateT-v0", n_envs=config["n_envs"], env_kwargs={"config": env_config}
+    )
 
-model_save_path = os.path.join(experiment_path, "models")
-os.makedirs(model_save_path, exist_ok=True)
+    # Select the model based on the algorithm argument
+    if config["algorithm"] == "PPO":
+        model = PPO(
+            config["policy_type"],
+            env,
+            verbose=1,
+            gamma=config["gamma"],
+            tensorboard_log=wandb.run.dir,
+        )
+    elif config["algorithm"] == "DQN":
+        model = DQN(
+            config["policy_type"],
+            env,
+            verbose=1,
+            gamma=config["gamma"],
+            tensorboard_log=wandb.run.dir,
+        )
+    else:
+        raise ValueError(f"Unsupported algorithm: {config['algorithm']}")
 
-# Define the model with the WandB callback specified in the learn method
-model = PPO(
-    "MlpPolicy", env, verbose=1, gamma=config["gamma"], tensorboard_log=logs_path
-)
+    # Start training with frequent checkpoints
+    model.learn(
+        total_timesteps=config["n_total_timesteps"],
+        callback=WandbCallback(
+            gradient_save_freq=config["checkpoint_freq"],
+            model_save_freq=config["checkpoint_freq"],
+            model_save_path=wandb.run.dir,  # Save models in the WandB run directory
+            verbose=2,
+        ),
+        progress_bar=True,
+    )
 
-# Start training
-model.learn(
-    total_timesteps=config["n_total_timesteps"],
-    callback=WandbCallback(
-        gradient_save_freq=1,
-        model_save_path=model_save_path,
-        verbose=2,
-    ),
-    progress_bar=True,
-)
+    # Close the WandB run
+    run.finish()
 
-# After training, you might want to evaluate the model
-mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
-wandb.log({"mean_reward": mean_reward, "std_reward": std_reward})
 
-# Close the WandB run
-run.finish()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Train a NavigateT environment with WandB."
+    )
+    parser.add_argument(
+        "--algorithm", type=str, default="PPO", help="RL algorithm to use (PPO or DQN)"
+    )
+    parser.add_argument(
+        "--policy_type",
+        type=str,
+        default="MlpPolicy",
+        help="Policy type (e.g., MlpPolicy)",
+    )
+    parser.add_argument(
+        "--total_timesteps",
+        type=int,
+        default=30_000_000,
+        help="Total timesteps for training",
+    )
+    parser.add_argument("--n_envs", type=int, default=1, help="Number of environments")
+    parser.add_argument("--gamma", type=float, default=1, help="Discount factor")
+    parser.add_argument(
+        "--width", type=int, default=32, help="Width of the environment"
+    )
+    parser.add_argument(
+        "--height", type=int, default=24, help="Height of the environment"
+    )
+    parser.add_argument(
+        "--control_interval", type=float, default=1e-3, help="Control interval"
+    )
+    parser.add_argument("--gravity", type=float, default=1.5, help="Gravity value")
+    parser.add_argument(
+        "--initial_velocity", type=float, default=100, help="Initial velocity"
+    )
+    parser.add_argument(
+        "--max_iterations",
+        type=int,
+        default=int(1e4),
+        help="Max iterations per episode",
+    )
+    parser.add_argument(
+        "--checkpoint_freq",
+        type=int,
+        default=10000,
+        help="Checkpoint frequency for saving the model",
+    )
+    parser.add_argument(
+        "--project",
+        type=str,
+        default="lvmc-navigateT-v0.1.0",
+        help="WandB project name",
+    )
 
-# Run the trained model for one episode and log the video
-obs = env.reset()
-for _ in range(500):
-    action, _ = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
-    env.render(
-        mode="console"
-    )  # this will render the environment in the console using rich library
-    if done:
-        break
-
-    # We should save the output
+    args = parser.parse_args()
+    main(args)
