a02dfc8af2bad42efcaecea7186914c31a3b7e26
zakarya <zakaria.elkhiyati@gmail.com>
2024-07-20 16:05:18 +0200

    Refactor to reflect the new desired T shapped environment

--- envs/episodic/navigate_T.py
+++ envs/episodic/navigate_T.py
@@ -1,8 +1,10 @@
 # The task will be for a number of  particles to navigate a T shape and go upwards instead of downwards.
 
-from envs.base_env import LVMCBaseEnv
-from lvmc.core.particle_lattice import Orientation
+from envs.base_env import LVMCBaseEnv, Orientation, EnvConfig, Optional
+
+# from lvmc.core.particle_lattice import Orientation
 import torch
+import numpy as np
 
 
 class NavigateT(LVMCBaseEnv):
@@ -10,78 +12,106 @@ class NavigateT(LVMCBaseEnv):
     Environment for navigating a T shape.
     """
 
-    def reset(self, seed=None):
-        obs, _ = super().reset(seed)
-
-        self.simulation.add_particle_flux(
-            region=self.init_region,
-            orientation=Orientation.RIGHT,
-            n_particles=self.n_init_particles,
+    def _initialize_simulation(self, seed: Optional[int]) -> None:
+        super()._initialize_simulation(seed=seed)
+        obstacles, sinks, region, region_size = self.make_T_shape(self.config)
+        self.simulation.add_obstacles(obstacles).add_sinks(sinks).add_particles(
+            region=region, orientation=Orientation.RIGHT
         )
-        self.n_top = 0
-        self.n_down = 0
+        self.simulation.build()
         self.n_particles = self.simulation.lattice.n_particles
-
-        obs = self._get_obs()
-        return obs, {}
+        self.n_init = self.n_particles
+        self.info["n_particles_survived"] = 0
+        self.info["n_particles_lost"] = 0
+        self.info.update(
+            {
+                "n_particles_escaped": 0,
+                "exit_reward": 0,
+                "completion_bonus": 0,
+                "time_penalty": 0,
+                "n_init": self.n_init,
+            }
+        )
+        self.info
+        self.logger.info("Environment initialized")
 
     def reward(self):
+        pos = np.array(list(self.simulation.lattice.id_to_position.values()))
+        n_survived = np.sum(pos[:, 1] == 0)
+        n_lost = np.sum(pos[:, 1] == self.config.height - 1)
+        n_escaped = np.sum(pos[:, 0] == 0)
 
-        if self.n_particles == self.simulation.lattice.n_particles:
-            return -0.001
+        n_good_exit = n_survived - self.info["n_particles_survived"]
+        n_bad_exit = (
+            n_lost
+            - self.info["n_particles_lost"]
+            + (n_escaped - self.info["n_particles_escaped"])
+        )
 
-        self.n_particles = self.simulation.lattice.n_particles
-        pos = self.simulation.lattice.id_to_position  # dictionary of particle positions
+        self.info["n_particles_survived"] = n_survived
+        self.info["n_particles_lost"] = n_lost
+        self.info["n_particles_escaped"] = n_escaped
 
-        # now compute the number of particles that have reached the top of the lattice
-        n_top = 0
-        n_down = 0
-        for p in pos:
+        # Calculate percentage of particles that have exited correctly
 
-            if pos[p][1] == 0:
-                n_top += 1
-            elif pos[p][1] == self.height - 1:
-                n_down += 1
+        correct_exit_percentage = self.info["n_particles_survived"] / self.n_init
 
-        reward = (self.n_top - n_top) - (self.n_down - n_down)
-        self.n_top = n_top
-        self.n_down = n_down
-        return reward / self.n_init_particles
+        # Graduated completion bonus
+        completion_bonus = 0
+        if self.is_done():
+            completion_bonus = correct_exit_percentage
+
+        exit_reward = (n_good_exit - n_bad_exit) / self.n_init
+        time_penalty = -1 / self.config.max_iterations
+
+        w1 = 0.0
+        w2 = 1.0
+        w3 = 0.0
+
+        reward = w1 * completion_bonus + w2 * exit_reward + w3 * time_penalty
+
+        self.info["exit_reward"] += exit_reward
+        self.info["completion_bonus"] += completion_bonus
+        self.info["time_penalty"] += time_penalty
+
+        return reward
 
     def is_done(self):
         return self.simulation.lattice.is_empty.item()
 
-    def compute_lattice_topology(self) -> dict:
-        return make_T_shape(self.width, self.height)
+    @staticmethod
+    def make_T_shape(config: EnvConfig):
+        """
+        Create a T shape for the environment.
+        Args:
+            config (EnvConfig): Configuration parameters for the environment.
 
+        Returns:
+            Tuple containing:
+            - obstacles (torch.Tensor): Boolean tensor representing obstacles.
+            - sinks (torch.Tensor): Boolean tensor representing sinks.
+            - init_region (List[int]): Coordinates defining the initial region [x1, x2, y1, y2].
+            - region_size (int): Size of the initial region.
+        """
+        cutoff = int(
+            (config.height - 1) * config.t_shape_cutoff
+        )  # Use 30% of the lattice height as an upper/bottom wall.
+        depth = int((config.width - 1) * config.t_shape_depth)
 
-def make_T_shape(width, height):
-    """
-    Create a T shape for the environment.
-    """
-    cutoff = int(
-        (height - 1) * 0.4
-    )  # Use 30% of the lattice height as an upper/bottom wall.
-    depth = int((width - 1) * 0.9)
+        obstacles = torch.zeros((config.height, config.width), dtype=torch.bool)
+        sinks = torch.zeros((config.height, config.width), dtype=torch.bool)
+        sinks[0] = 1
+        sinks[-1] = 1
+        sinks[cutoff : config.height - cutoff, 0] = 1
+        obstacles[:cutoff, 0:depth] = 1
+        obstacles[-cutoff:, 0:depth] = 1
+        obstacles[:, -1] = 1
+        x1, x2, y1, y2 = 1, depth, cutoff, config.height - cutoff - 1
+        init_region = [x1, x2, y1, y2]
 
-    obstacles = torch.zeros((height, width), dtype=torch.bool)
-    sinks = torch.zeros((height, width), dtype=torch.bool)
-    sinks[0] = 1
-    sinks[-1] = 1
-    obstacles[:cutoff, 0:depth] = 1
-    obstacles[-cutoff:, 0:depth] = 1
-    obstacles[:, -1] = 1
-    x1, x2, y1, y2 = 0, depth, cutoff, height - cutoff - 1
-    init_region = [x1, int(x2 / 5), y1, y2]
+        # compute the number of particles to be added as a function of the initial region size
+        region_size = (init_region[1] - init_region[0] + 1) * (
+            init_region[3] - init_region[2] + 1
+        )
 
-    # compute the number of particles to be added as a function of the initial region size
-    region_size = (init_region[1] - init_region[0] + 1) * (
-        init_region[3] - init_region[2] + 1
-    )
-    n_init_particles = int(region_size * 0.9)
-    return {
-        "sinks": sinks,
-        "obstacles": obstacles,
-        "init_region": init_region,
-        "n_init_particles": n_init_particles,
-    }
+        return obstacles, sinks, init_region, region_size
